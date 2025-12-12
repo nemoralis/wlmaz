ac43286992f3cd44dd714f6257eea69b89699852
Nemoralis <nemoralis@duck.com>
2025-12-12 16:12:52 +0400

    prepare for upload

--- vite.config.ts
+++ vite.config.ts
@@ -143,6 +143,10 @@ export default defineConfig({
             target: "http://localhost:3000",
             changeOrigin: true,
          },
+         "/upload": {
+            target: "http://localhost:3000",
+            changeOrigin: true,
+         },
       },
    },
    esbuild: {
