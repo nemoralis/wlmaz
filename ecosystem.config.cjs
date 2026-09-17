0d810b7e343be594c8be6871955a7b56f20ccbee
Nemoralis <nemoralis@duck.com>
2026-09-17 12:19:18 +0400

    add date to pm2 logs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
     script: "./src/index.ts",
     interpreter: "./node_modules/.bin/tsx",
     node_args: "--env-file=.env",
+    log_date_format: "YYYY-MM-DD HH:mm:ss.SSS",
     env: {
       NODE_ENV: "production",
       PORT: 3000
